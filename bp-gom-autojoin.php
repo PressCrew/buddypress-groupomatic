b6ea030cc6dc15cbe774f6c05a949e9cb475e751
Marshall Sorenson <marshall@presscrew.com>
2012-11-28 12:02:50 -0500

    hook directly into xprofile data saving instead of form processes

--- bp-gom-autojoin.php
+++ bp-gom-autojoin.php
@@ -1,9 +1,19 @@
 <?php
 
-function bp_gom_autojoin_maybe_accept_invite( $user_id )
+function bp_gom_autojoin_maybe_accept_invite( $xprofile_data )
 {
-	// get all matching group ids for the user
-	$user_groups_meta = bp_gom_matching_groups_meta( $user_id, false );
+	// make sure data is the right type of object
+	if ( $xprofile_data instanceof BP_XProfile_ProfileData ) {
+		// set vars
+		$user_id = $xprofile_data->user_id;
+		$field_id = $xprofile_data->field_id;
+	} else {
+		// nope, bail
+		return;
+	}
+	
+	// get all matching group ids for the user and field
+	$user_groups_meta = bp_gom_matching_groups_meta( $user_id, $field_id, false );
 
 	// loop all matched groups
 	foreach ( $user_groups_meta->get_groups() as $user_group_meta ) {
@@ -14,7 +24,6 @@ function bp_gom_autojoin_maybe_accept_invite( $user_id )
 		}
 	}
 }
-add_action( 'bp_core_signup_user', 'bp_gom_autojoin_maybe_accept_invite', 11 );
-add_action( 'xprofile_updated_profile', 'bp_gom_autojoin_maybe_accept_invite', 11 );
+add_action( 'xprofile_data_after_save', 'bp_gom_autojoin_maybe_accept_invite' );
 
 ?>
